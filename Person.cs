d1d422f93c878094cf762b680d13347c427f39f0
evohat3 <118646438+evohat3@users.noreply.github.com>
2026-07-31 01:58:27 -0500

    Serialization Demo

---
+++ Person.cs
@@ -0,0 +1,8 @@
+namespace Serialization
+{
+    public class Person
+    {
+        public string? UserName { get; set; }
+        public int UserAge { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.IO;
+using System.Xml.Serialization;
+using System.Text.Json;
+
+namespace Serialization
+{
+    
+    public class Program
+    {
+        
+        static void Main()
+        {
+            Person personOne = new Person {UserName = "JimmyNeutron", UserAge = 12};
+
+            // *** Serialize to Binary ***
+            // using (FileStream fs = new FileStream("Person.dat", FileMode.Create))
+            // {
+            //     BinaryWriter writer = new BinaryWriter(fs);
+            //     writer.Write(personOne.UserName);
+            //     writer.Write(personOne.UserAge);
+            // }
+
+            // Console.WriteLine("Binary Serialization Complete");
+
+            // *** Serialize XML ***
+            // XmlSerializer xml = new XmlSerializer(typeof(Person));
+            // using (StreamWriter writer = new StreamWriter("person.xml"))
+            // {
+            //     xml.Serialize(writer,personOne);
+            // }
+            // Console.WriteLine("XML Serialization Completed");
+
+            // *** Serialize Json ***
+
+            string jsonString = JsonSerializer.Serialize(personOne);
+
+            File.WriteAllText("person.json", jsonString);
+
+            Console.WriteLine("JSON Serialization Completed");
+        
+        }
+
+
+    }
+
+}
